d83b3244f593aa350d56f709b71739e11bee880c
amirkogit <amirkomail@gmail.com>
2016-11-11 10:44:15 -0500

    Moved ComException handling classes to the Precompiled header file

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -3,40 +3,6 @@
 
 using namespace Microsoft::WRL;
 
-struct ComException
-{
-    HRESULT result;
-
-    explicit ComException(HRESULT const value) :
-        result(value)
-    {
-
-    }
-};
-
-inline void HR(HRESULT const result)
-{
-    _ASSERTE(S_OK == result);
-
-    if (S_OK != result)
-    {
-        throw ComException(result);
-    }
-}
-
-struct ComRuntime
-{
-    ComRuntime()
-    {
-        HR(CoInitializeEx(nullptr,
-                          COINIT_MULTITHREADED));
-    }
-    ~ComRuntime()
-    {
-        CoUninitialize();
-    }
-};
-
 int main()
 {
     ComRuntime runtime;
--- Client/Precompiled.h
+++ Client/Precompiled.h
@@ -1,3 +1,37 @@
 #pragma once
 
 #include <wrl.h>
+
+struct ComException
+{
+    HRESULT result;
+
+    explicit ComException(HRESULT const value) :
+        result(value)
+    {
+
+    }
+};
+
+inline void HR(HRESULT const result)
+{
+    _ASSERTE(S_OK == result);
+
+    if (S_OK != result)
+    {
+        throw ComException(result);
+    }
+}
+
+struct ComRuntime
+{
+    ComRuntime()
+    {
+        HR(CoInitializeEx(nullptr,
+            COINIT_MULTITHREADED));
+    }
+    ~ComRuntime()
+    {
+        CoUninitialize();
+    }
+};
