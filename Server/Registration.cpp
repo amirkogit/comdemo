ae3510e6f7920678f0129773de542c540db98089
amirkogit <amirkomail@gmail.com>
2016-11-14 14:28:16 -0500

    Changed the project settings to test Register and UnRegister DLL functions

--- Server/Registration.cpp
+++ Server/Registration.cpp
@@ -1,5 +1,14 @@
 #include "Precompiled.h"
 
+// Note: Change the following in the Project Settings to test register and unregister functions
+//Server Property pages :
+// -Debugging :
+//	Command : C : \Windows\SysWow64\regsvr32.exe
+//	Command Arguments : $(TargetPath) // To test the register function
+//	Command Arguments : / u $(Target Path) // To test unregister function
+
+
+
 HRESULT __stdcall DllRegisterServer()
 {
     return S_OK;
