23a6293e0ad288a2da6f7aaf8a9d43ee07b74231
amirkogit <amirkomail@gmail.com>
2016-11-14 11:04:00 -0500

    Creating class objects on the Client side
    - Running at this state doesn't work because the Server component has
    not been registered !

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -6,4 +6,18 @@ using namespace Microsoft::WRL;
 int main()
 {
     ComRuntime runtime;
+
+    ComPtr<IHatchery> hatchery;
+
+    HR(CoGetClassObject(__uuidof(Hen),
+                        CLSCTX_INPROC_SERVER,
+                        nullptr,
+                        __uuidof(hatchery),
+                        reinterpret_cast<void **>(hatchery.GetAddressOf())));
+
+    ComPtr<IHen> hen;
+
+    HR(hatchery->CreateHen(hen.GetAddressOf()));
+
+    hen->Cluck();
 }
