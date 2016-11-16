adaf10218800caceea5480347c8b2dedd342438e
amirkogit <amirkomail@gmail.com>
2016-11-16 15:50:05 -0500

    demo: using remote interface
    - currently throwing exception saying that the CLASS_NOT_REGISTERED !

--- Server/Server.h
+++ Server/Server.h
@@ -12,10 +12,8 @@ struct __declspec(uuid("c1b11d4a-12f1-400b-87bb-95d52a4c5021"))
 };
 
 // Replacing with IClassFactory
-#if 0
 struct __declspec(uuid("3cb31964-dd2b-4793-9455-ae796180d218"))
     IHatchery : IUnknown
 {
     virtual HRESULT __stdcall CreateHen(IHen ** hen) = 0;
 };
-#endif // 0
